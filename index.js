2b9c55415cc5ac41dc50c8797fd5dc6c1630a5cb
Sy <stephanetraore46@gmail.com>
2024-01-22 11:09:42 -0500

    challenge 4 step 1 solved

--- index.js
+++ index.js
@@ -70,10 +70,9 @@ const contributionsCard = document.getElementById("num-contributions");
 const totalContribution = GAMES_JSON.reduce((acc, game) => {
     return acc + game.backers;
 }, 0);
-contributionsCard = totalContribution;
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
-
+contributionsCard.innerHTML = `${totalContribution.toLocaleString()}`;
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
